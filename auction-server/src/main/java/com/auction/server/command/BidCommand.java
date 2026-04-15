51436d6f7d4000db81e228abfe8350128f9d5f31
nguyentrongnhan <25020301@vnu.edu.vn>
2026-04-15 17:23:45 +0700

    feat(Command): add bidcommand

--- auction-server/src/main/java/com/auction/server/command/BidCommand.java
+++ auction-server/src/main/java/com/auction/server/command/BidCommand.java
@@ -10,6 +10,9 @@ public class BidCommand implements Command{
     public Response execute(Object data, String requestId, User u){
         BidTransaction bidTransaction = (BidTransaction) data;
         Response res = AuctionManager.getInstance().processBid(bidTransaction);
+        if(res == null){
+            return new Response(requestId, null, null, null);
+        }
         return new Response(requestId, res.getStatus(), res.getMessage(), res.getPayload());
     }
 }
