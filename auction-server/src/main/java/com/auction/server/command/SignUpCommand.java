09907c315d48c87614b184d11dbcd6ba23dadfb6
nguyentrongnhan <25020301@vnu.edu.vn>
2026-04-16 23:12:50 +0700

    refactor(command): tách trách nhiệm request signup khỏi clienthandler

---
+++ auction-server/src/main/java/com/auction/server/command/SignUpCommand.java
@@ -0,0 +1,23 @@
+package com.auction.server.command;
+
+import com.auction.server.dao.UserDao;
+import com.auction.shared.Request;
+import com.auction.shared.Response;
+import com.auction.shared.User;
+
+public class SignUpCommand implements Command{
+    @Override
+    public Response execute(Object data, String requestId, User u){
+        try {
+            User res = (User) data;
+            boolean res2 = UserDao.getInstance().signup(res);
+            return  new Response(requestId, res2 ? Response.OK : Response.ERROR, res2 ? "success" : "duplicate", null);
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+            return new Response(requestId, Response.ERROR, "class_cast_error", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Response(requestId, Response.ERROR, "server_error", null);
+        }
+    }
+}
