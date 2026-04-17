41bdaac51253813c49081787288d6cd706a6e3a9
nguyentrongnhan <25020301@vnu.edu.vn>
2026-04-18 00:23:17 +0700

    feat(command): nhập getbidhistory vào commands

--- auction-server/src/main/java/com/auction/server/Service/RequestDispatcher.java
+++ auction-server/src/main/java/com/auction/server/Service/RequestDispatcher.java
@@ -1,6 +1,8 @@
 package com.auction.server.Service;
 
 import com.auction.server.command.*;
+import com.auction.server.command.commands.*;
+import com.auction.server.command.queries.*;
 import com.auction.shared.Request;
 import com.auction.shared.Response;
 import com.auction.shared.User;
@@ -13,13 +15,14 @@ public class RequestDispatcher {
     static {
         commands.put(Request.LOGIN, new LoginCommand());
         commands.put(Request.ADD_LOT, new AddLotCommand());
-        commands.put(Request.GET_PENDING_ITEMS, new GetPendingItemsCommand());
+        commands.put(Request.GET_PENDING_ITEMS, new GetPendingItemsQuery());
         commands.put(Request.APPROVE_ITEM, new ApproveItemCommand());
-        commands.put(Request.GET_ONGOING_BIDS, new GetOnGoingBidsCommand());
-        commands.put(Request.GET_ALL_USERS,new GetAllUserCommand());
-        commands.put(Request.GET_ITEM_BY_ID, new GetItemByIdCommand());
+        commands.put(Request.GET_ONGOING_BIDS, new GetOnGoingBidsQuery());
+        commands.put(Request.GET_ALL_USERS,new GetAllUserQuery());
+        commands.put(Request.GET_ITEM_BY_ID, new GetItemByIdQuery());
         commands.put(Request.BID, new BidCommand());
         commands.put(Request.SIGNUP, new SignUpCommand());
+        commands.put("get_bid_history", new GetBidHistoryQuery());
     }
 
     public static Response dispatch(Request req, User u){
