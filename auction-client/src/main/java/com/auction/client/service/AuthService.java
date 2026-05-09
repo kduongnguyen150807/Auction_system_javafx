40616636007b4f8b49b44d48656519d3343b7900
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-10 00:00:11 +0700

    refactor: add generic type parameter to request handler

--- auction-client/src/main/java/com/auction/client/service/AuthService.java
+++ auction-client/src/main/java/com/auction/client/service/AuthService.java
@@ -17,27 +17,21 @@ public final class AuthService {
   private AuthService() {
   }
 
-  public void login(LoginCredentials credentials,
-                    Consumer<Response<User>> callback, Consumer<Throwable> errorCallback) {
-
-    RequestHelper.sendRequest(RequestType.LOGIN, credentials,
-
-      response -> handleLoginResponse((Response<User>) response, callback),
-
-      errorCallback);
+  public void login(LoginCredentials credentials, Consumer<Response<User>> callback, Consumer<Throwable> errorCallback) {
+    RequestHelper.<User>sendRequest(RequestType.LOGIN, credentials,
+      response -> {
+        handleLoginResponse(response);
+        callback.accept(response);
+      }, errorCallback);
   }
 
-  private void handleLoginResponse(Response<User> response, Consumer<Response<User>> callback) {
-
+  private void handleLoginResponse(Response<User> response) {
     if (response.getStatus() == ResponseStatus.SUCCESS) {
-
       User user = response.getData();
-
       ClientSession.setCurrentUser(user);
     }
-
-    callback.accept(response);
   }
+
 
   public static AuthService getInstance() {
 
