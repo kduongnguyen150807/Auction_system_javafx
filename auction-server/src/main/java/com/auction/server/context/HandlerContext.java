57bfde03d1e211155995f36cc0105ab7c9403bc0
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-10 00:05:35 +0700

    refactor(context): HandlerContext now only hold ClientHandler and user info

--- auction-server/src/main/java/com/auction/server/context/HandlerContext.java
+++ auction-server/src/main/java/com/auction/server/context/HandlerContext.java
@@ -4,17 +4,11 @@ import com.auction.server.controller.ClientHandler;
 import com.auction.shared.user.User;
 
 public class HandlerContext {
-  private final DaoContext daoContext;
   private final ClientHandler clientHandler;
   private User user;
 
-  public HandlerContext(DaoContext daoContext, ClientHandler clientHandler) {
-    this.daoContext = daoContext;
+  public HandlerContext(ClientHandler clientHandler) {
     this.clientHandler = clientHandler;
-  }
-
-  public DaoContext getDaoContext() {
-    return daoContext;
   }
 
   public User getUser() {
