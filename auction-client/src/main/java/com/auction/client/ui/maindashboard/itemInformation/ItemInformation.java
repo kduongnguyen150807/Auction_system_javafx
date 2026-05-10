11c96ac1015927d0d39870112783ec701f5acbc3
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-11 00:46:43 +0700

    refactor(itemInformation): split iteminformation into 3 part, main layout, infolayout and buttonlayout

---
+++ auction-client/src/main/java/com/auction/client/ui/maindashboard/itemInformation/ItemInformation.java
@@ -0,0 +1,10 @@
+package com.auction.client.ui.maindashboard.itemInformation;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class ItemInformation extends VBox {
+
+  @FXML private ItemInfoLayout infoLayout;
+  @FXML private ItemButtonLayout buttonLayout;
+}
