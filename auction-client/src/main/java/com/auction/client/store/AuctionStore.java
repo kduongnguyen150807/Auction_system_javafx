f42ace0bb94704dbdf4818b13fe42560acbd15e0
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-14 17:05:39 +0700

    refactor(store): migrate cache logic to store

--- auction-client/src/main/java/com/auction/client/store/AuctionStore.java
+++ auction-client/src/main/java/com/auction/client/store/AuctionStore.java
@@ -2,6 +2,8 @@ package com.auction.client.store;
 
 import com.auction.shared.item.Item;
 import com.auction.shared.item.ItemStatus;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.slf4j.Logger;
@@ -13,6 +15,20 @@ public class AuctionStore {
   private static final Logger LOGGER = LoggerFactory.getLogger(AuctionStore.class);
 
   private final ObservableList<Item> items = FXCollections.observableArrayList();
+
+  private final ObjectProperty<Item> focusedItem = new SimpleObjectProperty<>();
+
+  public Item getFocusedItem() {
+    return focusedItem.get();
+  }
+
+  public void setFocusedItem(Item item) {
+    focusedItem.set(item);
+  }
+
+  public ObjectProperty<Item> focusedItemProperty() {
+    return focusedItem;
+  }
 
   public ObservableList<Item> filterStatus(ItemStatus status) {
     List<Item> list = items.stream().filter(item -> item.getStatus().equals(status)).toList();
