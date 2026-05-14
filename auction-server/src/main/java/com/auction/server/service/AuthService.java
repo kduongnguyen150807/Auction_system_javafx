48a9d802d045f722604b9b7358ef19f3fe55968c
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-14 17:17:43 +0700

    refactor(AuthService): make sure register rollback if login fail

--- auction-server/src/main/java/com/auction/server/service/AuthService.java
+++ auction-server/src/main/java/com/auction/server/service/AuthService.java
@@ -1,38 +1,53 @@
 package com.auction.server.service;
 
 import com.auction.server.context.HandlerContext;
-import com.auction.server.dao.UserDao;
+import com.auction.server.dao.transaction.TransactionManager;
+import com.auction.server.repository.UserRepository;
 import com.auction.shared.dto.LoginCredentials;
+import com.auction.shared.dto.RegisterCredentials;
 import com.auction.shared.linkv2.Response;
 import com.auction.shared.user.User;
 import com.auction.shared.user.UserStatus;
 
 public class AuthService {
-  private final UserDao userDao;
+  private final UserRepository userRepository;
 
-  public AuthService(UserDao userDao) {
-    this.userDao = userDao;
+  public AuthService(UserRepository userRepository) {
+    this.userRepository = userRepository;
   }
 
   public Response<User> login(String requestId, LoginCredentials loginCredentials, HandlerContext handlerContext) {
-    User user = userDao.login(loginCredentials.getUsername(), loginCredentials.getPassword());
-
+    User user = TransactionManager.read(connection -> {
+      return userRepository.login(loginCredentials.getUsername(), loginCredentials.getPassword(), connection);
+    });
     if (user == null) {
-      return Response.error(
-        requestId,
-        "Invalid username or password"
-      );
+      return Response.error(requestId, "Invalid password or username");
     }
 
     if (user.getStatus() == UserStatus.LOCKED) {
-      return Response.error(
-        requestId,
-        "Account has been locked"
-      );
+      return Response.error(requestId, "You are not allowed to login");
     }
 
     handlerContext.setUser(user);
+    return Response.success(requestId, "success", user);
+  }
 
-    return Response.success(requestId, "Login successful", user);
+  public Response<User> register(String requestId, RegisterCredentials credentials, HandlerContext handlerContext) {
+    User user = TransactionManager.execute(conn -> {
+      userRepository.register(
+        credentials.getUsername(),
+        credentials.getPassword(),
+        credentials.getEmail(),
+        credentials.getAge(),
+        conn);
+      return userRepository.login(credentials.getUsername(), credentials.getPassword(), conn);
+    });
+
+    if (user == null) {
+      return Response.error(requestId, "Failed to register user");
+    } else {
+      handlerContext.setUser(user);
+      return Response.success(requestId, "success", user);
+    }
   }
 }
