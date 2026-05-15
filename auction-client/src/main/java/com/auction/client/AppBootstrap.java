3d090361b4c11ac8fa6ee032ba7421e7750de4a0
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-15 23:11:08 +0700

    refactor(app-bootstrap): setup SceneManager and initial SceneType routing for login flow

---
+++ auction-client/src/main/java/com/auction/client/AppBootstrap.java
@@ -0,0 +1,23 @@
+package com.auction.client;
+
+import com.auction.client.navigation.SceneManager;
+import com.auction.client.navigation.SceneType;
+import com.auction.client.ui.homeview.HomeView;
+import com.auction.client.ui.loginview.LoginView;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppBootstrap {
+  public static void run(Stage stage) {
+    SceneManager sceneManager = SceneManager.getInstance();
+    sceneManager.setStage(stage);
+
+    /* khoi tao scene */
+    Scene loginViewScene = new Scene(new LoginView());
+
+    /* register scene */
+    sceneManager.registerScene(SceneType.LOGIN_VIEW, loginViewScene);
+
+    sceneManager.switchScene(SceneType.LOGIN_VIEW);
+  }
+}
