92788670cf44f81504350c0417ccb8579f826a46
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-19 01:53:09 +0700

    refactor(UserService): migrate changing account information logic to UserService

---
+++ auction-client/src/main/java/com/auction/client/service/UserService.java
@@ -0,0 +1,65 @@
+package com.auction.client.service;
+
+import com.auction.client.network.NetworkClient;
+import com.auction.client.store.ClientSession;
+import com.auction.client.util.RequestHelper;
+import com.auction.shared.Request;
+import com.auction.shared.Response;
+import com.auction.shared.User;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService {
+  public static String updateProfile(String fullName, String email, String phone) {
+    Map<String, String> data = new HashMap<>();
+    data.put("userid", String.valueOf(ClientSession.CURRENT_SESSION.getCurrentUser().getId()));
+    data.put("fullname", fullName);
+    data.put("email", email);
+    data.put("phone", phone);
+    return RequestHelper.sendRequest(Request.UPDATE_PROFILE, (Serializable) data)
+      .thenApply(response -> {
+        if (response != null && response.getStatus().equals(Response.OK)) {
+          ClientSession.CURRENT_SESSION.applyProfileUpdate(fullName, email, phone);
+          return null;
+        } else {
+          return "Fail to update profile";
+        }
+      }).join();
+  }
+
+  public static String deposit(double amount) {
+    Map<String, String> data = new HashMap<>();
+    data.put("userid", String.valueOf(ClientSession.CURRENT_SESSION.getCurrentUser().getId()));
+    data.put("amount", String.valueOf(amount));
+    return RequestHelper.sendRequest(Request.DEPOSIT, (Serializable) data)
+      .thenApply(response -> {
+        if (response != null && response.getStatus().equals(Response.OK)) {
+          ClientSession.CURRENT_SESSION.deposit(amount);
+          return null;
+        } else {
+          return "Fail to deposit amount";
+        }
+      }).join();
+  }
+
+  public static String updateAvatar(String username, String avatarUrl) {
+    Request request = new Request(Request.UPDATE_AVATAR, username + " " + avatarUrl);
+    Response response = NetworkClient.getInstance().sendRequestAndWait(request);
+    if (response != null && Response.OK.equals(response.getStatus())) {
+      ClientSession.CURRENT_SESSION.avatarUrlProperty().set(avatarUrl);
+      return null;
+    }
+    return "fail to update avatar";
+  }
+
+  public static String uploadImage(String uploadUrl, byte[] imageBytes) {
+    try {
+      String url = NetworkClient.uploadFile(uploadUrl, imageBytes);
+      return updateAvatar(ClientSession.CURRENT_SESSION.getCurrentUser().getUsername(), url);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
